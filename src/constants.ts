b68ec522f15bc3c8d54ebc17f9c48e8092f3b4f9
margaiwangara <margaiwangara@gmail.com>
2022-01-30 14:03:07 +0300

    added styling and visuals

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,6 @@
-export const __BASE_URL =
+export const __BASE_URL__ =
   process.env.NODE_ENV === 'production'
     ? 'https://ms-tsjs.herokuapp.com'
     : 'http://localhost:5000';
+export const __MODEL_URL__ =
+  'https://raw.githubusercontent.com/PnCheck/model/main/models/js_model/model.json';
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,19 @@
 import styles from '@/css/Home.module.css';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import Image from 'next/image';
 import axios from 'axios';
-import { __BASE_URL } from '@/constants';
+import { __BASE_URL__, __MODEL_URL__ } from '@/constants';
+import Head from 'next/head';
+import * as tf from '@tensorflow/tfjs';
 
 function HomePage() {
   const [selectedFile, setSelectedFile] = useState<File>();
   const [file, setFile] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [prediction, setPrediction] = useState();
+  const uploaderRef = useRef<HTMLInputElement>(null);
+
+  let model;
 
   const handleFileUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
     try {
@@ -17,11 +24,20 @@ function HomePage() {
       const formData = new FormData();
       formData.append('file', e.target.files[0]);
 
-      const response = await axios.post(`${__BASE_URL}/upload`, formData, {
+      if (!model) model = await tf.loadLayersModel(__MODEL_URL__);
+
+      const response = await axios.post(`${__BASE_URL__}/upload`, formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
       });
+
+      const data = response.data;
+
+      const processedImage = tf.tensor2d(data);
+      const result = model.predict(tf.reshape(processedImage, [1, 28, 28, 1]));
+      setPrediction(result);
+      // const label = prediction.argMax(1).get([0]);
 
       alert(response.data.message);
     } catch (error) {
@@ -29,30 +45,60 @@ function HomePage() {
     }
   };
 
+  const handleClick = () => {
+    if (uploaderRef?.current) {
+      uploaderRef.current.click();
+    }
+  };
+
   return (
-    <main className={styles.wrapper}>
-      <section className={styles.inner}>
-        <section className={styles.start}>
-          <form method="POST" className={styles.form}>
-            <input accept="image/*" type="file" onChange={handleFileUpload} />
-            <section className={styles.view}>
-              {file && (
-                <Image
-                  src={{
-                    src: file,
-                    width: 300,
-                    height: 300,
-                  }}
-                  alt="xray of a human thorax"
-                  objectFit="cover"
-                />
-              )}
-            </section>
-          </form>
+    <>
+      <Head>
+        <title>PnCheck | Home</title>
+      </Head>
+      <main className={styles.wrapper}>
+        {loading && (
+          <section className={styles.overlay}>
+            <div className={styles.spinner} />
+          </section>
+        )}
+        <section className={styles.inner}>
+          <h3 className={styles.title}>PnCheck</h3>
+          <section className={styles.start}>
+            <form method="POST" className={styles.form}>
+              <input
+                accept="image/*"
+                type="file"
+                onChange={handleFileUpload}
+                style={{ visibility: 'hidden' }}
+                ref={uploaderRef}
+              />
+              <button
+                className={styles.uploader}
+                onClick={handleClick}
+                type="button"
+              >
+                Click to Upload File
+              </button>
+              <section className={styles.view}>
+                {file && (
+                  <Image
+                    src={{
+                      src: file,
+                      width: 300,
+                      height: 300,
+                    }}
+                    alt="xray of a human thorax"
+                    objectFit="cover"
+                  />
+                )}
+              </section>
+              {prediction && <h3 className="result">{prediction}</h3>}
+            </form>
+          </section>
         </section>
-        {/* <section className={styles.end}></section> */}
-      </section>
-    </main>
+      </main>
+    </>
   );
 }
 
