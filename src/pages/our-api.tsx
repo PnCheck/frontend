ec0024c5d35970e5c64fa4e40f17714e4dc832e4
margaiwangara <margaiwangara@gmail.com>
2022-01-31 21:31:10 +0300

    our api page description text added

--- src/pages/our-api.tsx
+++ src/pages/our-api.tsx
@@ -6,11 +6,7 @@ function OurAPI() {
     <DefaultLayout title="Our API">
       <h3 className={styles.title}>Coming Soon!</h3>
       <p className={styles.description}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore qui,
-        deleniti quam possimus iusto reiciendis pariatur quis ut temporibus quos
-        modi odit accusamus sint expedita, architecto ipsam non? Facilis
-        dignissimos error reiciendis aliquid dolore quisquam magnam, molestiae
-        repellat excepturi quod.
+        We're currently working on bringing API integration...
       </p>
     </DefaultLayout>
   );
