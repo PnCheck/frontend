7629dc412c176e8c703828d5e37fb619885bcb4f
margaiwangara <margaiwangara@gmail.com>
2022-01-31 19:07:24 +0300

    modified info page to include description

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -1,5 +1,6 @@
 import styles from '@/css/Info.module.css';
 import DefaultLayout from '@/containers/DefaultLayout';
+import Image from 'next/image';
 
 function Info() {
   return (
@@ -9,6 +10,23 @@ function Info() {
         able to make client-side predictions for the classification of chest
         X-ray images to having or not having Pneumonia
       </p>
+      <h3 className={styles.title}>Examples</h3>
+      <section className={styles.display}>
+        <section className={styles.box}>
+          <Image
+            src="/images/normal1.jpeg"
+            alt="normal chest x-ray"
+            layout="fill"
+          />
+        </section>
+        <section className={styles.box}>
+          <Image
+            src="/images/pneumonia2.jpeg"
+            alt="pneumonic chest x-ray"
+            layout="fill"
+          />
+        </section>
+      </section>
     </DefaultLayout>
   );
 }
